0c65c169f886872af080bdb30e435c9688b86576
rbazgan <122384399+rbazgan@users.noreply.github.com>
2023-10-13 13:26:42 +0300

    modify

---
+++ my-realtime-chatapp_backend/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+
+
+  try {
+     const r = await axios.put(
+'https://api.chatengine.io/users/',
+{
+    username: username,
+    secret: username,
+    first_name:username
+},
+{headers: {"private-key":"a2700b16-3983-404d-b685-0e57162a281b"}}
+)
+return res.status(r.status).json(r.data)
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data)
+  }
+});
+
+app.listen(3001);
